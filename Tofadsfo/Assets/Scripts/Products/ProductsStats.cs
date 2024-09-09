8808a3506972af3776210502b136d552a7cc3f7f
Limuwahanelis <37908839+Limuwahanelis@users.noreply.github.com>
2024-09-09 17:26:45 +0200

    Make tables display number of associated ingredients.

--- Tofadsfo/Assets/Scripts/Products/ProductsStats.cs
+++ Tofadsfo/Assets/Scripts/Products/ProductsStats.cs
@@ -1,9 +1,11 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class ProductsStats : MonoBehaviour
 {
+    public Action<ProductSO, int> OnCurrentProductAmountChanged;
     [SerializeField] List<ProductSO> _products=new List<ProductSO>();
     [SerializeField] List<int> _maxAmounts;
     [SerializeField] List<int> _currentAmounts;
@@ -17,6 +19,18 @@ public class ProductsStats : MonoBehaviour
     {
         int index=_products.IndexOf(product);
         return (_maxAmounts[index], _currentAmounts[index]);
+    }
+    public void ReduceProductAmount(ProductSO product,int amount)
+    {
+        int index = _products.IndexOf(product);
+        _currentAmounts[index] -= amount;
+        OnCurrentProductAmountChanged?.Invoke(product, _currentAmounts[index]);
+    }
+    public void OncreaseProductAmount(ProductSO product, int amount)
+    {
+        int index = _products.IndexOf(product);
+        _currentAmounts[index] += amount;
+        OnCurrentProductAmountChanged?.Invoke(product, _currentAmounts[index]);
     }
 
 }
---
+++ Tofadsfo/Assets/Scripts/Tables/TableWithProducts.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class TableWithProducts : MonoBehaviour,IInteractable
+{
+    public ProductSO AssociatedProdct => _associatedProduct;
+    public int MaxProductAmount => _maxProductAmount;
+    [SerializeField] ProductSelect _productListUI;
+    [SerializeField] SpriteRenderer _productSprite;
+    [SerializeField] int _maxProductAmount;
+    [SerializeField] TMP_Text _productAmountDisplay;
+    [SerializeField] ProductsStats _productStats;
+    [SerializeField] TableProductsManagment _tableProductsManagment;
+    private ProductSO _associatedProduct;
+    private int _currentProductAmount;
+    public void Interact()
+    {
+        ShowProductSelection();
+    }
+    public void ShowProductSelection()
+    {
+        _productListUI.ShowList(this);
+    }
+    public void SetProduct(ProductSO product)
+    {
+        _productSprite.sprite = product.Icon;
+        //_tableProductsManagment.RemoveProductFromTable(product, this);
+        _tableProductsManagment.AddProductToATable(product, this);
+        _associatedProduct = product;
+    }
+    public void SetProductAmount(int amount)
+    {
+        _currentProductAmount = amount;
+        _productAmountDisplay.text = _currentProductAmount.ToString();
+    }
+
+}
