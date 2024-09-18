5e71037ca70966e6b6e7b6a284b3073ec5b89b0d
Limuwahanelis <37908839+Limuwahanelis@users.noreply.github.com>
2024-09-18 21:09:07 +0200

    Add converting recipe to short format in RecipeSO.

--- Tofadsfo/Assets/Scripts/ScriptableObjects/RecipeSO.cs
+++ Tofadsfo/Assets/Scripts/ScriptableObjects/RecipeSO.cs
@@ -1,8 +1,10 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
-[CreateAssetMenu(fileName ="New recipe",menuName ="Recipe")]
+[CreateAssetMenu(fileName = "New recipe", menuName = "Recipe")]
 public class RecipeSO : ScriptableObject
 {
     public struct CraftingRecipeShort
@@ -11,10 +13,47 @@ public class RecipeSO : ScriptableObject
         public int[] resourcesNum;
     }
     public int Price => _price;
+    public CraftingRecipeShort ShortForm
+    {
+        get
+        {
+            if (_shortForm.productTypes == null)
+            {
+                _shortForm = ConvertRecipetoShortFormat();
+                return _shortForm;
+            }
+            else return _shortForm;
+        }
+    }
     public ProductSO Result => _result;
     public List<ProductSO> Ingredients => _ingredients;
     public Sprite Icon => _result.Icon;
+
+    private CraftingRecipeShort _shortForm;
     [SerializeField] ProductSO _result;
     [SerializeField] List<ProductSO> _ingredients;
     [SerializeField] int _price;
+
+    CraftingRecipeShort ConvertRecipetoShortFormat()
+    {
+        RecipeSO.CraftingRecipeShort shortRecipe = new RecipeSO.CraftingRecipeShort();
+        int numberOfDistinctResources = Ingredients.Distinct().Count();
+        ProductSO[] types = new ProductSO[numberOfDistinctResources];
+        int[] num = new int[numberOfDistinctResources];
+        int j = 0;
+        for (int i = 0; i < Ingredients.Count(); i++)
+        {
+            if (types.Contains(Ingredients[i])) continue;
+            types[j] = Ingredients[i];
+            j++;
+        }
+        for (int i = 0; i < Ingredients.Count(); i++)
+        {
+            int index = Array.IndexOf(types, Ingredients[i]);
+            num[index]++;
+        }
+        shortRecipe.resourcesNum = num;
+        shortRecipe.productTypes = types;
+        return shortRecipe;
+    }
 }
