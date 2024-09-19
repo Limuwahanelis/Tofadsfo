88a55250de92f964fda291a8cb251bd4d9b69d01
Limuwahanelis <37908839+Limuwahanelis@users.noreply.github.com>
2024-09-19 02:01:44 +0200

    Make snap to gird execute only in editor.

--- Tofadsfo/Assets/Scripts/Misc/SnapToGrid.cs
+++ Tofadsfo/Assets/Scripts/Misc/SnapToGrid.cs
@@ -7,8 +7,10 @@ public class SnapToGrid : MonoBehaviour
 {
 
     // Update is called once per frame
+#if UNITY_EDITOR
     void Update()
     {
         transform.position = new Vector2(Mathf.Round(transform.position.x), Mathf.Round(transform.position.y));
     }
+#endif
 }
